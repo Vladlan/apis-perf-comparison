c6ce5934778723af588472238f0cc01be3d6018f
Vladlan <lanvlad911@gmail.com>
2024-09-10 16:14:08 +0200

    Update build.gradle.kts

--- posts-api-kotlin/build.gradle.kts
+++ posts-api-kotlin/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.3.3"
 	id("io.spring.dependency-management") version "1.1.6"
+	kotlin("plugin.jpa") version "1.9.25"
 }
 
 group = "com.example"
@@ -33,3 +34,4 @@ kotlin {
 		freeCompilerArgs.addAll("-Xjsr305=strict")
 	}
 }
+
